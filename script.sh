2b0f16cc979c44b2c03c5b91c9757428851e8025
www.CLANPUTOS.org <par4na@gmail.com>
2019-03-07 19:26:38 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-sudo su root
 sed -i 's/^Port .*/Port 22/g' /etc/ssh/sshd_config
 sed -i 's/^PermitRootLogin .*/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
